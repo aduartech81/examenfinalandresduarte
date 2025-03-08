6a2fb7d9d8306e29c597068ee7e6fd780130abbe
aduartech81 <aduartech@gmail.com>
2025-03-07 21:10:42 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo "Inicializando configuración en Streamlit..."
+python3 -m venv env
+source env/bin/activate
 pip install --upgrade pip
 pip install plotly pandas streamlit matplotlib
